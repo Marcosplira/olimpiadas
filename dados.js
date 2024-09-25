419c4ff58a625fbda3eb67c5223c0c68da117e16
Marcosplira <61232814+Marcosplira@users.noreply.github.com>
2024-09-25 13:46:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, a Fadinha, é um fenômeno do skate mundial. Com apenas [idade], a brasileira já conquistou diversos títulos e se tornou um ícone da modalidade, inspirando jovens skatistas em todo o mundo.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8" 
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma das maiores promessas do judô brasileiro. A atleta, com sua técnica impecável e força, conquistou diversas medalhas em competições nacionais e internacionais, e é considerada uma das principais candidatas a medalhas olímpicas.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é um nome que brilha no cenário da ginástica artística mundial. A brasileira, com seu talento inegável e determinação, conquistou o coração dos brasileiros e se tornou a maior medalhista olímpica do país.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade", 
+      tags: "ginástica ginastica ouro"
+    }
+  ];
